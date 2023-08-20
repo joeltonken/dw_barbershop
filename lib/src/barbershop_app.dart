f1b92c5117a2b8a211d405af9cf3ecfcf53cab4a
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-08-20 19:59:20 -0300

    implemented user sign up screen

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -3,6 +3,7 @@ import 'package:dw_barbershop/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
+import 'package:dw_barbershop/src/features/auth/register/user_register_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +24,8 @@ class BarbershopApp extends StatelessWidget {
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (_) => const LoginPage(),
+            '/auth/register/user': (_) => const UserRegisterPage(),
+            '/auth/register/barbershop': (_) => const Center(child: Text('Barbershop page')),
             '/home/adm': (_) => const Text('ADM'),
             '/home/employee': (_) => const Text('Employee'),
           },
