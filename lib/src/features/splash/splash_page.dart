fc1640208a735b2ff3bcb48e19a691440d46c5cb
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-08-22 13:19:17 -0300

    fix animation: timing and transition adjustments

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:dw_barbershop/src/core/ui/constants.dart';
@@ -8,24 +9,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SplashPage extends ConsumerStatefulWidget {
-
-  const SplashPage({ super.key });
+  const SplashPage({super.key});
 
   @override
   ConsumerState<SplashPage> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends ConsumerState<SplashPage> {
-
   var _scale = 10.0;
   var _animationOpacityLogo = 0.0;
 
   double get _logoAnimationWidth => 100 * _scale;
   double get _logoAnimationHeight => 120 * _scale;
 
+  var endAnimation = false;
+  Timer? redirectTimer;
+
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_) { 
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
         _animationOpacityLogo = 1.0;
         _scale = 1;
@@ -34,79 +36,71 @@ class _SplashPageState extends ConsumerState<SplashPage> {
     super.initState();
   }
 
-   @override
-   Widget build(BuildContext context) {
-       ref.listen(splashVmProvider, (_, state) {
-        state.whenOrNull(
-          error: (error, stackTrace) {
-            log('Erro ao validar o login', error: error, stackTrace: stackTrace);
-            Messages.showError('Erro ao validar o login', context);
-            Navigator.of(context).pushNamedAndRemoveUntil('/auth/login', (route) => false);
-          },
-          data: (data) {
-            switch(data) {
-              case SplashState.loggedADM:
-                Navigator.of(context).pushNamedAndRemoveUntil('/home/adm', (route) => false);
-              case SplashState.loggedEmployee:
-                Navigator.of(context).pushNamedAndRemoveUntil('/home/employee', (route) => false);  
-              case _:
-                Navigator.of(context).pushNamedAndRemoveUntil('/home/login', (route) => false);
-            }
-          }
-        );
+  void _redirect(String routeName) {
+    if (!endAnimation){
+      redirectTimer?.cancel();
+      redirectTimer = Timer(const Duration(milliseconds: 300), (){
+        _redirect(routeName);
       });
+    } else {
+      redirectTimer?.cancel();
+      Navigator.of(context).pushNamedAndRemoveUntil(routeName, (route) => false);
+    }
+  }
 
-       return Scaffold(
-           backgroundColor: Colors.black,
-           body: DecoratedBox(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
+  @override
+  Widget build(BuildContext context) {
+    ref.listen(splashVmProvider, (_, state) {
+      state.whenOrNull(error: (error, stackTrace) {
+        log('Erro ao validar o login', error: error, stackTrace: stackTrace);
+        Messages.showError('Erro ao validar o login', context);
+        _redirect('/auth/login');
+      }, data: (data) {
+        switch (data) {
+          case SplashState.loggedADM:
+            _redirect('/home/adm');
+          case SplashState.loggedEmployee:
+            _redirect('/home/employee');
+          case _:
+            _redirect('/auth/login');
+        }
+      });
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
                 ImageConstants.backgroundChair,
               ),
               opacity: 0.2,
-              fit: BoxFit.cover
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeIn,
+            opacity: _animationOpacityLogo,
+            onEnd: () {
+              setState(() {
+                endAnimation = true;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 3),
+              width: _logoAnimationWidth,
+              height: _logoAnimationHeight,
+              curve: Curves.linearToEaseOut,
+              child: Image.asset(
+                ImageConstants.imageLogo,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          child: Center(
-            child: AnimatedOpacity(
-              duration: const Duration(seconds: 1),
-              curve: Curves.easeIn,
-              opacity: _animationOpacityLogo,
-              onEnd: () {
-                Navigator.of(context).pushAndRemoveUntil(
-                  PageRouteBuilder(
-                    settings: const RouteSettings(name: '/auth/login'),
-                    pageBuilder: (
-                    context,
-                    animation,
-                    secondaryAnimation,
-                    ) {
-                      return const LoginPage();
-                    },
-                    transitionsBuilder: (_, animation, __, child) {
-                      return FadeTransition(
-                        opacity: animation,
-                         child: child,
-                         );
-                    },
-                    ), 
-                    (route) => false,
-                    );
-              },
-              child: AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                width: _logoAnimationWidth,
-                height: _logoAnimationHeight,
-                curve: Curves.linearToEaseOut,
-                child: Image.asset(
-                  ImageConstants.imageLogo, 
-                  fit: BoxFit.cover,
-                  ),
-                  ),
-                ),
-          ),
         ),
-       );
+      ),
+    );
   }
 }
